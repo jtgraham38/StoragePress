007384a58e8f9c1d8629b2ab49acbc73f8cb59c2
jtgraham38 <jtgraham38@gmail.com>
2024-03-12 15:04:20 -0400

    Update storage unit size inputs to use number type

--- elements/storage_unit_meta_inputs.php
+++ elements/storage_unit_meta_inputs.php
@@ -9,9 +9,9 @@
 <div class="sp_input_group">
     <label class="storagepress_input_label" for="sp_size">Size:</label>
     <div id="sp_size"  style="display: flex; flex-direction: row; align-items: center;">
-        <input class="storagepress_settings_input" type="text" id="sp_length" name="sp_length" value="<?php echo isset($post->ID) && get_post_meta($post->ID, 'sp_length', true) ? esc_attr(get_post_meta($post->ID, 'sp_length', true)) : ''; ?>"size="25" />
+        <input class="storagepress_settings_input" type="number" min="0" step="0.25" id="sp_length" name="sp_length" value="<?php echo isset($post->ID) && get_post_meta($post->ID, 'sp_length', true) ? esc_attr(get_post_meta($post->ID, 'sp_length', true)) : ''; ?>"size="25" />
         <span>&times;</span>
-        <input class="storagepress_settings_input" type="text" id="sp_width" name="sp_width" value="<?php echo isset($post->ID) && get_post_meta($post->ID, 'sp_width', true) ? esc_attr(get_post_meta($post->ID, 'sp_width', true)) : ''; ?>"size="25" />
+        <input class="storagepress_settings_input" type="number" min="0" step="0.25" id="sp_width" name="sp_width" value="<?php echo isset($post->ID) && get_post_meta($post->ID, 'sp_width', true) ? esc_attr(get_post_meta($post->ID, 'sp_width', true)) : ''; ?>"size="25" />
         <select name="sp_unit" id="sp_unit" style="margin-left: 0.25rem;">
             <option value="ft" <?php echo isset($post->ID) && get_post_meta($post->ID, 'sp_unit', true) == 'ft' ? 'selected' : ''; ?>>ft</option>
             <option value="m" <?php echo isset($post->ID) && get_post_meta($post->ID, 'sp_unit', true) == 'm' ? 'selected' : ''; ?>>m</option>
