a2a835b4f1d15613c262413545890bd6b939dcb9
jacob <jacobgraham@woosterbrush.com>
2024-03-14 11:27:51 -0400

    fixed removing unsaved features.

--- elements/features_options_field.php
+++ elements/features_options_field.php
@@ -34,6 +34,28 @@ if (!defined('ABSPATH')) {
         color: red;
     }
     
+    #storagepress_add_feature_option_container{
+        display: inline-flex;
+        align-items: center;
+        border: 1px solid #ccc;
+        border-radius: 0.5rem;
+    }
+    #storagepress_add_feature_option_button {
+        border: none;
+        padding-left: 1rem;
+        font-size: larger;
+        background-color: transparent;
+        display: grid;
+        place-items: center;
+    }
+    #storagepress_add_feature_option_button:hover {
+        background-color: #2271b1;
+    }
+    #storagepress_add_feature_option {
+        border: none;
+        border-top-left-radius: 0.5rem;
+        border-bottom-left-radius: 0.5rem;
+    }
 </style>
 
 <?php
@@ -45,7 +67,7 @@ $options = get_option('storagepress_feature_options', []);
 <template id="feature_option_template">
     <div class="feature_option" id="storagepress_feature_option_{{key}}">
         <label for="storagepress_feature_options[]">{{value}}</label>
-        <button type="button" id="" onclick="document.getElementById('storagepress_feature_option_{{key}}').remove()">&times;</button>
+        <button type="button">&times;</button>
         <input type="hidden" name="storagepress_feature_options[]" value="{{value}}">
     </div>
 </template>
@@ -59,16 +81,27 @@ $options = get_option('storagepress_feature_options', []);
     document.getElementById('storagepress_add_feature_option_button').addEventListener('click', function() {
         let feature_option = document.getElementById('storagepress_add_feature_option').value;
         if(feature_option) {
-            let new_feature_option= document.getElementById('feature_option_template').content.cloneNode(true);
-            console.log(new_feature_option)
-            new_feature_option.querySelector('label').textContent = feature_option;
-            new_feature_option.querySelector('input').value = feature_option;
-            new_feature_option.querySelector('div').id = 'storagepress_feature_option_' + feature_option;
-            new_feature_option.addEventListener('click', function() {
-                document.getElementById('storagepress_feature_option_' + feature_option).remove();
+
+            //generate a random id
+            var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            let id_str = ''
+            for (var i = 0; i < 16; i++) {
+                id_str += characters.charAt(Math.floor(Math.random() * characters.length));
+            }
+
+            //clone the template and add it to the container
+            let new_feature_option_input = document.importNode(document.getElementById('feature_option_template').content, true)
+            console.log(new_feature_option_input)
+            new_feature_option_input.querySelector('label').textContent = feature_option;
+            new_feature_option_input.querySelector('input').value = feature_option;
+            new_feature_option_input.querySelector('div').id = 'storagepress_feature_option_' + feature_option + "_" + id_str;
+            new_feature_option_input.querySelector('button').addEventListener('click', function(event) {
+                event.target.parentNode.remove()
             });
-            document.getElementById('storagepress_feature_options_container').appendChild(new_feature_option);
+            
+            document.getElementById('storagepress_feature_options_container').appendChild(new_feature_option_input);
             document.getElementById('storagepress_add_feature_option').value = '';
+
         }
     });
 </script>
