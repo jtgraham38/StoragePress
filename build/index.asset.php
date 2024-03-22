ca090eccd44f28a6a73ff09f363829978b48810d
jacob <jacobgraham@woosterbrush.com>
2024-03-22 14:41:21 -0400

    refactored unit meta block into its own file.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react'), 'version' => '608a9ba13155b320e4b1');
+<?php return array('dependencies' => array('react'), 'version' => '7f217cbebc48d9592039');
---
+++ src/storage_unit_meta_block.js
@@ -0,0 +1,31 @@
+//wp is registered in the browser global scop by WordPress
+export default wp.blocks.registerBlockType(
+    "storagepress/storage-unit-meta-block", // Unique name of the block
+    {
+        title: "Storage Unit Meta", // title of the block
+        icon: "smiley", // dashicon to show in the admin panel
+        category: "typography", // category of the block
+        attributes: {
+            key: { type: "string" },
+        },  // attributes of the block
+        edit: function (props) {
+            return (
+                <span>
+                    <select
+                    value={props.attributes.key}
+                    onChange={(event)=>{ 
+                        props.setAttributes({ key: event.target.value })
+                     }}>
+                        <option value="sp_size">Size</option>
+                        <option value="sp_price">Price</option>
+                        <option value="sp_features">Features</option>
+                        <option value="sp_available">Availability</option>
+                    </select>
+                </span>
+            )
+        }, // function to render the block in the editor (admin appearance)
+        save: function (props) {
+            return null // return null to dynomically render the block content with php
+        }, // function to save the block content (front-end appearance)
+    },  // configuration object for the block
+)
