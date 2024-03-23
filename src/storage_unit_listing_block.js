ceef3a1353e19182a69546c90cf3d99a4f587345
Jacob <jtgraham38@gmail.com>
2024-03-23 14:55:33 -0400

    Added stubs for other blocks.

--- src/storage_unit_listing_block.js
+++ src/storage_unit_listing_block.js
@@ -1,27 +1,19 @@
+const InnerBlocks = wp.blockEditor; // InnerBlocks is registered in the browser global scop by WordPress
+
 //wp is registered in the browser global scop by WordPress
 export default wp.blocks.registerBlockType(
-    "storagepress/storage-unit-meta-block", // Unique name of the block
+    "storagepress/storage-unit-listing-block", // Unique name of the block
     {
-        title: "Storage Unit Meta", // title of the block
+        title: "Storage Unit Listing", // title of the block
         icon: "vault", // dashicon to show in the admin panel
-        category: "storagepress", // category of the block
+        category: "storagepress", // category of the block 
         attributes: {
             key: { type: "string", default: 'none' },    //once published, don't change the name of an attribute, could lead to errors with previously created blocks
         },  // attributes of the block
         edit: function (props) {
             return (
                 <span>
-                    <select
-                    value={props.attributes.key}
-                    onChange={(event)=>{ 
-                        props.setAttributes({ key: event.target.value })
-                     }}>
-                        <option value="none">Choose a Unit Meta...</option>
-                        <option value="sp_size">Size</option>
-                        <option value="sp_price">Price</option>
-                        <option value="sp_features">Features</option>
-                        <option value="sp_available">Availability</option>
-                    </select>
+                   Unit Listing
                 </span>
             )
         }, // function to render the block in the editor (admin appearance)
@@ -30,3 +22,6 @@ export default wp.blocks.registerBlockType(
         }, // function to save the block content (front-end appearance)
     },  // configuration object for the block
 )
+//
+////TODO: finish
+//
