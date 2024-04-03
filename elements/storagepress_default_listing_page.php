732640bc3254d3bad6b23adb88a6b8c3276bbda3
jacob <jacobgraham@woosterbrush.com>
2024-04-03 15:38:51 -0400

    Updated default listing page.

--- elements/storagepress_default_listing_page.php
+++ elements/storagepress_default_listing_page.php
@@ -1,41 +1,64 @@
 <!-- wp:storagepress/storage-unit-business-detail-block {"key":"storagepress_name","fontSize":"large"} /-->
 
-<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group"><!-- wp:site-logo /-->
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"20%"} -->
+<div class="wp-block-column" style="flex-basis:20%"><!-- wp:site-logo {"shouldSyncIcon":true} /--></div>
+<!-- /wp:column -->
 
-<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
-<div class="wp-block-group"><!-- wp:heading {"level":5} -->
-<h5 class="wp-block-heading">Become a Customer</h5>
+<!-- wp:column {"width":"40%"} -->
+<div class="wp-block-column" style="flex-basis:40%"><!-- wp:heading {"level":4} -->
+<h4 class="wp-block-heading">Become a Customer</h4>
 <!-- /wp:heading -->
 
-<!-- wp:storagepress/storage-unit-business-detail-block {"key":"storagepress_rental_terms"} /-->
+<!-- wp:separator {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"className":"is-style-wide"} -->
+<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide" style="margin-top:0;margin-bottom:0"/>
+<!-- /wp:separator -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0"},"margin":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"},"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20);padding-top:0;padding-bottom:0"><!-- wp:heading {"level":5} -->
+<h5 class="wp-block-heading">Make Checks Payable To:</h5>
+<!-- /wp:heading -->
 
 <!-- wp:storagepress/storage-unit-business-detail-block {"key":"storagepress_checks_payable_to"} /-->
 
-<!-- wp:paragraph -->
-<p></p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:group -->
+<!-- wp:spacer {"height":"0px","style":{"layout":{"flexSize":"1rem","selfStretch":"fixed"}}} -->
+<div style="height:0px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
 
-<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
-<div class="wp-block-group"><!-- wp:heading {"level":5} -->
-<h5 class="wp-block-heading">Contact</h5>
+<!-- wp:details {"style":{"spacing":{"blockGap":"0"}},"className":"is-style-default","fontSize":"medium"} -->
+<details class="wp-block-details is-style-default has-medium-font-size"><summary>Rental Terms</summary><!-- wp:storagepress/storage-unit-business-detail-block {"key":"storagepress_rental_terms"} /--></details>
+<!-- /wp:details --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"40%"} -->
+<div class="wp-block-column" style="flex-basis:40%"><!-- wp:heading {"level":4} -->
+<h4 class="wp-block-heading">Contact</h4>
 <!-- /wp:heading -->
+
+<!-- wp:separator {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"className":"is-style-wide"} -->
+<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide" style="margin-top:0;margin-bottom:0"/>
+<!-- /wp:separator -->
 
 <!-- wp:storagepress/storage-unit-business-detail-block {"key":"storagepress_address"} /-->
 
 <!-- wp:storagepress/storage-unit-business-detail-block {"key":"storagepress_phone"} /-->
 
 <!-- wp:storagepress/storage-unit-business-detail-block {"key":"storagepress_email"} /--></div>
-<!-- /wp:group --></div>
-<!-- /wp:group -->
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Storage Units:</h3>
+<!-- /wp:heading -->
 
 <!-- wp:query {"queryId":9,"query":{"perPage":3,"pages":0,"offset":0,"postType":"sp_storage_units","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"parents":[]}} -->
 <div class="wp-block-query"><!-- wp:post-template -->
-<!-- wp:post-title /-->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10","left":"var:preset|spacing|20","right":"var:preset|spacing|20"}},"border":{"width":"1px"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group" style="border-width:1px;padding-top:var(--wp--preset--spacing--10);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--10);padding-left:var(--wp--preset--spacing--20)"><!-- wp:post-title /-->
 
-<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group"><!-- wp:post-featured-image /-->
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"0","left":"0","top":"0","bottom":"0"},"blockGap":"var:preset|spacing|40"},"border":{"width":"0px","style":"none"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group" style="border-style:none;border-width:0px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:post-featured-image {"width":"60%","style":{"border":{"radius":"0px"}}} /-->
 
 <!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
 <div class="wp-block-group"><!-- wp:storagepress/storage-unit-meta-block {"key":"sp_size"} /-->
@@ -44,7 +67,14 @@
 
 <!-- wp:storagepress/storage-unit-meta-block {"key":"sp_available"} /-->
 
-<!-- wp:storagepress/storage-unit-meta-block {"key":"sp_features"} /--></div>
+<!-- wp:storagepress/storage-unit-meta-block {"key":"sp_features"} /-->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Reserve</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 <!-- /wp:post-template -->
