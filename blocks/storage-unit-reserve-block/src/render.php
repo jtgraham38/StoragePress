50086bce9c15f48876423bce22dff3b31b2cfa03
jacob <jacobgraham@woosterbrush.com>
2024-04-05 14:59:10 -0400

    Implemented frontend modal hide and show on button press.

--- blocks/storage-unit-reserve-block/src/render.php
+++ blocks/storage-unit-reserve-block/src/render.php
@@ -1,1 +1,11 @@
-<button <?php get_block_wrapper_attributes() ?> onclick="console.log('REserve!')">Reserve</button>
+
+<div>
+    <span <?php echo get_block_wrapper_attributes(array('class'=>'storagepress-reserve-button')) ?> onclick="storagepress_reserve_unit_<?php the_ID(); ?>_modal.showModal()">
+        Reserve
+    </span>
+
+    <dialog id="storagepress_reserve_unit_<?php the_ID(); ?>_modal">
+        <button onclick="storagepress_reserve_unit_<?php the_ID(); ?>_modal.close()" class="storagepress-reserve-modal-close-button">&times;</button>
+        <h4>Reserve Storage Unit "<?php the_title() ?>"</h4>
+    </dialog>
+</div>
