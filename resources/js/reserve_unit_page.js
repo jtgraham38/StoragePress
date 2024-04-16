d38257ec4d2a0de64c5521f45c8996c2000ee492
jacob <jacobgraham@woosterbrush.com>
2024-04-16 16:35:16 -0400

    Remove unused JavaScript file

--- resources/js/reserve_unit_page.js
+++
@@ -1,3 +0,0 @@
-/* this script is enqueued on the frontend, t help facilitate interactions between storagepress blocks */
-
-//TODO: a better notification syste than window.alert would be ideal, and also a way to handle updates without a page reload
